4c9f7fc6c7f95d7b1fde84b3a74193411c84d0fb
eeneroth <eric.eneroth@topofminds.se>
2019-10-18 14:46:38 +0200

    Added new simulator.

---
+++ citrus-simulator-schibsted/src/main/java/com/consol/citrus/simulator/scenario/CurrencyConverterScenario2.java
@@ -0,0 +1,51 @@
+package com.consol.citrus.simulator.scenario;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Eric Eneroth
+ * accelerate_bos2019
+ */
+
+@Scenario("CurrencyConverter")
+//@RequestMapping(value = "/jarvso/currencyconverter", method = RequestMethod.POST)
+@RequestMapping(value = "/services/rest/simulator/currencyconverter/latest", method = RequestMethod.GET)
+
+public class CurrencyConverterScenario extends AbstractSimulatorScenario {
+
+    @Override
+    public void run(ScenarioDesigner scenario) {
+/*
+        scenario
+                .http()
+                .receive()
+                .post()
+                .payload("<Lenny></Lenny>");
+
+
+
+        scenario
+                .http()
+                .receive()
+                .get("/services/rest/simulator/currencyconverter/latest")
+                .queryParam("base");
+*/
+
+        //echo("hello world!");
+
+        scenario
+                .http()
+                .send()
+                .response(HttpStatus.OK)
+                .payload("{\n" +
+                        "\t\"rates\": {\n" +
+                        "\t\t\"USD\": 1.1025,\n" +
+                        "\t\t\"GBP\": 0.8656\n" +
+                        "\t},\n" +
+                        "\t\"base\": \"EUR\",\n" +
+                        "\t\"date\": \"2019-10-16\"\n" +
+                        "}");
+    }
+}
